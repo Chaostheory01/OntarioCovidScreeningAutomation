e493d4fbb1efa81b5c6cb70ec7e863f832e65640
Greg <athaide.greg@gmail.com>
2022-01-07 08:16:12 -0500

    Update for new screening

--- CovidScreeningApp/Program.cs
+++ CovidScreeningApp/Program.cs
@@ -12,22 +12,16 @@ namespace CovidScreeningApp
         static async Task Main(string[] args)
         {   
             using var webDriver = new ChromeDriver();
-            await ExecuteActionAndPause(() => webDriver.Navigate().GoToUrl(@"https://covid-19.ontario.ca/self-assessment/"));
-            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Start self-assessment']")).Click());
-            //await ExecuteActionAndPause(() => webDriver.FindElement(By.Id("guardian")).Click());
-            //await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Continue']")).Click());
+            await ExecuteActionAndPause(() => webDriver.Navigate().GoToUrl(@"https://covid-19.ontario.ca/school-screening/"));
+            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Start school screening']")).Click());
+            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Yes']")).Click());      
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            //await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            //await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            //await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Continue']")).Click());
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
             await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='No']")).Click());
-            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//button[text()='Save self-assessment result (PDF)']")).Click());
+            await ExecuteActionAndPause(() => webDriver.FindElement(By.XPath(@"//div[text()='Download result (PDF)']")).Click());
             await Task.Delay(TimeSpan.FromSeconds(10)); // wait for download to complete
         }
 
